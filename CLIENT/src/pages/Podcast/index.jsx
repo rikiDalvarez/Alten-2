fd9e21cb059e248bb46bc232acec39a7ddb83537
Ricardo <ric.dalvarez@hotmail.com>
2022-12-14 14:49:11 +0100

    fixing style

--- CLIENT/src/pages/Podcast/index.jsx
+++ CLIENT/src/pages/Podcast/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import Container from 'react-bootstrap/esm/Container'
+import Header from '../../components/Header'
 import PodcasterCard from "../../components/PodcasterCard"
 import PodcastList from '../../components/PodcastList'
 
 function Podcast() {
 	return (
-		<div>Podcast
+		<Container>
+			<Header></Header>
 			<PodcasterCard></PodcasterCard>
 			<h1>Episodes: 1</h1>
 			<PodcastList></PodcastList>
-		</div>
+		</Container>
 	)
 }
 
--- CLIENT/src/pages/PodcastEpisode/index.jsx
+++ CLIENT/src/pages/PodcastEpisode/index.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import Container from 'react-bootstrap/esm/Container'
+import Header from '../../components/Header'
+import PodcasterCard from '../../components/PodcasterCard'
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/esm/Row';
+
 
 function PodcastEpisode() {
 	return (
-		<div>PodcastEpisode</div>
+		<Container>
+			<Header></Header>
+			<Row>
+
+				<PodcasterCard></PodcasterCard>
+				<Card style={{ width: '18rem' }}>
+					<Card.Body>
+						<Card.Title>Card Title</Card.Title>
+						<Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+						<Card.Text>
+							Some quick example text to build on the card title and make up the
+							bulk of the card's content.
+						</Card.Text>
+						<Card.Link href="#">Card Link</Card.Link>
+						<Card.Link href="#">Another Link</Card.Link>
+					</Card.Body>
+				</Card>
+			</Row>
+
+		</Container>
+
 	)
 }
 
