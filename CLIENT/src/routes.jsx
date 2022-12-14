5e3f10ca71d29321b0dee4c60f0ff88ad449e913
Ricardo <ric.dalvarez@hotmail.com>
2022-12-14 12:08:22 +0100

    add podcast/podcastid and episode/id routes

--- CLIENT/src/routes.jsx
+++ CLIENT/src/routes.jsx
@@ -1,11 +1,21 @@
 import Home from './pages/Home';
 import { createBrowserRouter } from 'react-router-dom';
+import PodcastEpisode from './pages/PodcastEpisode';
+import Podcast from './pages/Podcast';
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <Home></Home>,
-	}
+	},
+	{
+		path: '/podcast/:podcastId',
+		element: <Podcast></Podcast>,
+	},
+	{
+		path: '/podcast/:podcastId/episode/:episodeId',
+		element: <PodcastEpisode></PodcastEpisode>,
+	},
 ]);
 
 export default router;
